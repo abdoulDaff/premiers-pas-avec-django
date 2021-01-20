9da20ab11d573edd05883ff229ed640c18bcf0e1
Abdoul <abdoulniallydaff@gmail.com>
2021-01-20 01:40:29 +0000

    First View

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create  views.
+
+
+def home_page(request):
+    return HttpResponse("Hello World")
