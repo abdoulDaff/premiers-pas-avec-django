64ff62940a7b5d32e72902df5a0cadb373e39990
Abdoul <abdoulniallydaff@gmail.com>
2021-01-20 16:02:44 +0000

    Add lead list and detail views

--- leads/views.py
+++ leads/views.py
@@ -5,7 +5,17 @@ from .models import Lead
 # Create views.
 
 
-def home_page(request):
+def lead_list(request):
     leads = Lead.objects.all()
-    context = {"leads": leads, }
-    return render(request, "second_page.html", context)
+    context = {
+        "leads": leads,
+    }
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead": lead,
+    }
+    return render(request, "leads/lead_detail.html", context)
