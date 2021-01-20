a319c2d0fd4dfdad9ea0d6f2a263dfe6a8370519
Abdoul <abdoulniallydaff@gmail.com>
2021-01-20 14:45:15 +0000

    add urls and namespaces

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import path
-from leads.views import home_page
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page)
+    path('leads/', include("leads.urls", namespace="leads")),
 ]
